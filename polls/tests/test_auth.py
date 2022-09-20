1543903f6a3518189459f62df7f7419c8b5eaa58
Kittiporn Theamnooch <kittiporn132@gmail.com>
2022-09-20 18:49:18 +0700

    add more testcases

--- polls/tests/test_auth.py
+++ polls/tests/test_auth.py
@@ -1,7 +1,27 @@
+import datetime
+
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
 from django.contrib.auth.models import User
+from ..models import Question, Vote
+
+
+def create_question(question_text, days, end_day=None):
+    """Create a question using the give parameter.
+
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    if end_day is None:
+        return Question.objects.create(
+            question_text=question_text, pub_date=time)
+    end_day = timezone.now() + datetime.timedelta(days=end_day)
+    return Question.objects.create(
+        question_text=question_text, pub_date=time, end_date=end_day)
 
 
 class AuthTest(TestCase):
@@ -26,3 +46,22 @@ class AuthTest(TestCase):
         response = self.client.post(url, login_data)
         self.assertEqual(302, response.status_code)  # redirected
         self.assertRedirects(response, reverse('polls:index'))  # should redirect to the index page
+
+    def test_auth_user_vote(self):
+        self.client.login(username=self.username, password=self.password)
+        question = create_question("auth_check", days=-1)
+        response = self.client.post(reverse('polls:vote', args=(question.id,)))
+        self.assertEqual(200, response.status_code)
+        self.client.logout()
+        response = self.client.post(reverse('polls:vote', args=(question.id,)))
+        self.assertEqual(302, response.status_code)
+
+    def test_one_user_vote(self):
+        self.client.login(username=self.username, password=self.password)
+        question = create_question("auth_check", days=-1)
+        choice1 = question.choice_set.create(choice_text='test1')
+        choice2 = question.choice_set.create(choice_text='test2')
+        self.client.post(reverse('polls:vote', args=(question.id,)), {'choice': choice1.id})
+        self.client.post(reverse('polls:vote', args=(question.id,)), {'choice': choice2.id})
+        self.assertEqual(Vote.objects.count(), 1)
+
